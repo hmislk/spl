77ec3bbb904ad0c7366a205675998552577ee0f5
Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>
2023-05-17 20:03:31 +0530

    Signed-off-by: Dr M H Buddhika Ariyaratne <buddhika.ari@gmail.com>

---
+++ src/main/java/org/ohmis/seriallistner/SerialListener.java
@@ -0,0 +1,48 @@
+package org.ohmis.seriallistner;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+public class SerialListener {
+
+    private static String portName = "com2"; // Change this to the appropriate port name
+    private static int baudRate = 9600; // Change this to the desired baud rate
+
+    public static void main(String[] args) {
+        SerialPort serialPort = SerialPort.getCommPort(portName);
+        serialPort.setBaudRate(baudRate);
+        serialPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                    return;
+                byte[] newData = new byte[serialPort.bytesAvailable()];
+                int numRead = serialPort.readBytes(newData, newData.length);
+                String receivedString = new String(newData);
+                System.out.println(receivedString);
+            }
+        });
+
+        if (serialPort.openPort()) {
+            System.out.println("Serial port opened successfully.");
+        } else {
+            System.out.println("Failed to open serial port.");
+            return;
+        }
+
+        // Keep the main thread alive
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
